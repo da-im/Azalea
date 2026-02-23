58cd8cac17f4459beb44e5c8503c331481302751
da-im <98754306+da-im@users.noreply.github.com>
2026-02-23 01:48:31 +0100

    Update vite.config.ts
    update repo

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/azalea-affairs-luxe/',
+  base: '/Azalea/',
   server: {
     host: "::",
     port: 8080,
