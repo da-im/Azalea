be608100761d4c7e625cb2f0d3b691f7473d2479
da-im <98754306+da-im@users.noreply.github.com>
2026-03-04 23:08:40 +0100

    Update GalleryPreview.tsx

--- src/components/home/GalleryPreview.tsx
+++ src/components/home/GalleryPreview.tsx
@@ -17,6 +17,7 @@ const galleryImages = [
   { src: safariBirthday1, title: "Safari Themed Birthday", type: 'Birthday', price: 450 },
   { src: weddingReception, title: 'Elegant Wedding Reception', type: 'Wedding', price: 1200 },
   { src: babyShowerBloom, title: 'Beautiful Baby Shower', type: 'Baby Shower', price: 380 },
+  { src: goldBirthday, title: 'Gold & Black Birthday Celebration', type: 'Birthday', price: 520 },
   { src: stageLight, title: '180W 60LED PAR Stage light', type: 'Rentals', price: 25 },
   { src: WallWasher, title: '336 LED stage wall washer', type: 'Rentals', price: 35 },
   
