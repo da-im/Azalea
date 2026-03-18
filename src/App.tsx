e2c60a4dc9fb304f4e0003d391c8f51d004ee7e4
da-im <98754306+da-im@users.noreply.github.com>
2026-03-18 18:35:12 +0100

    Update App.tsx
    router

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/Azalea">
+      <BrowserRouter basename="/">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/about" element={<About />} />
